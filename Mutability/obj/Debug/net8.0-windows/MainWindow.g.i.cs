8e761c7a713ddc8a98f05129d1c2cd340deff780
loolbime-design <loolbime@gmail.com>
2026-01-14 09:05:33 +0100

    change config

--- Mutability/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Mutability/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace LearnDrawing {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Mutability;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Mutability;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Mutability/obj/Debug/net8.0-windows/Mutability.AssemblyInfo.cs
+++ Mutability/obj/Debug/net8.0-windows/Mutability.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mutability")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de20df7d29181a77bef4b64cce142e16e42c176d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mutability")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mutability")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
